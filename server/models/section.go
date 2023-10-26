54b0a519e7e6651d8d1348c39ae52e27e17d41cf
bp40 <60870525+bp40@users.noreply.github.com.>
2023-10-26 14:24:21 +0700

    add struct for displaying sections on the web

--- server/models/section.go
+++ server/models/section.go
@@ -12,6 +12,22 @@ type Section struct {
 	RoomNumber    int    `json:"room_number" db:"room_number"`
 }
 
+type WebSection struct {
+	Id              string `json:"section_id" db:"section_id"`
+	CourseCode      string `json:"course_code" db:"course_code"`
+	CourseName      string `json:"course_name" db:"course_name"`
+	Credits         int    `json:"credits" db:"credits"`
+	Timeslot        int    `json:"timeslot_id" db:"timeslot_id"`
+	InstructorFname string `json:"instructor_fname" db:"first_name"`
+	InstructorLname string `json:"instructor_lname" db:"last_name"`
+	SectionNumber   int    `json:"section_number" db:"section_number"`
+	CurrentEnrolled int    `json:"current_enrolled" db:"current_enrolled"`
+	Capacity        int    `json:"max_students" db:"max_students"`
+	Semester        int    `json:"semester" db:"semester"`
+	Year            int    `json:"year" db:"year"`
+	RoomNumber      int    `json:"room_number" db:"room_number"`
+}
+
 type SimpleSection struct {
 	Id       string `db:"section_id"`
 	Timeslot int    `db:"timeslot_id"`
