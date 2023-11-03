091262026820f759bb1ccb9a9a7bb9204efdddb7
bp40 <60870525+bp40@users.noreply.github.com.>
2023-11-03 11:38:25 +0700

    implement course add button

--- client/registraion-site/src/components/RegistrationTable.jsx
+++ client/registraion-site/src/components/RegistrationTable.jsx
@@ -1,58 +1,58 @@
 const RegistrationTable = () => {
-    return (
-        <div className="overflow-x-auto p-4 bordered">
-            <table className="table table-zebra">
-                {/* head */}
-                <thead>
-                <tr>
-                    <th></th>
-                    <th>Course Code</th>
-                    <th>Course Name</th>
-                    <th>Availability</th>
-                    <th>Timeslot</th>
-                </tr>
-                </thead>
-                <tbody>
-                {/* row 1 */}
-                <tr>
-                    <th>1</th>
-                    <td>Cy Ganderton</td>
-                    <td>Quality Control Specialist</td>
-                    <td>
-                        <div className='badge badge-accent'> Available  </div>
-                    </td>
-                    <td>
-                        <div className='badge badge-warning'> Monday AM  </div>
-                    </td>
-                </tr>
-                {/* row 2 */}
-                <tr>
-                    <th>2</th>
-                    <td>Hart Hagerty</td>
-                    <td>Desktop Support Technician</td>
-                    <td>
-                        <div className='badge badge-accent'> Available  </div>
-                    </td>
-                    <td>
-                        <div className='badge badge-warning'> Monday AM  </div>
-                    </td>
-                </tr>
-                {/* row 3 */}
-                <tr>
-                    <th>3</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>
-                        <div className='badge badge-accent'> Available  </div>
-                    </td>
-                    <td>
-                        <div className='badge badge-warning'> Monday AM  </div>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  return (
+    <div className="overflow-x-auto p-4 bordered">
+      <table className="table table-zebra">
+        {/* head */}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Course Code</th>
+            <th>Course Name</th>
+            <th>Availability</th>
+            <th>Timeslot</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr>
+            <th>1</th>
+            <td>Cy Ganderton</td>
+            <td>Quality Control Specialist</td>
+            <td>
+              <div className="badge badge-accent"> Available </div>
+            </td>
+            <td>
+              <div className="badge badge-warning"> Monday AM </div>
+            </td>
+          </tr>
+          {/* row 2 */}
+          <tr>
+            <th>2</th>
+            <td>Hart Hagerty</td>
+            <td>Desktop Support Technician</td>
+            <td>
+              <div className="badge badge-accent"> Available </div>
+            </td>
+            <td>
+              <div className="badge badge-warning"> Monday AM </div>
+            </td>
+          </tr>
+          {/* row 3 */}
+          <tr>
+            <th>3</th>
+            <td>Brice Swyre</td>
+            <td>Tax Accountant</td>
+            <td>
+              <div className="badge badge-accent"> Available </div>
+            </td>
+            <td>
+              <div className="badge badge-warning"> Monday AM </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default RegistrationTable
+export default RegistrationTable;
--- client/registraion-site/src/components/SearchResult.jsx
+++ client/registraion-site/src/components/SearchResult.jsx
@@ -1,82 +1,122 @@
-import {sectionsAtom} from "../routes/search.jsx";
-import {useAtomValue} from "jotai";
+import { sectionsAtom } from "../routes/search.jsx";
+import { atom, useAtom, useAtomValue, useSetAtom } from "jotai";
+import { useEffect, useState } from "react";
 
 const SearchResult = () => {
+  const results = useAtomValue(sectionsAtom);
+  const [courseCart, setCourseCart] = useState([]);
+  const timeslotsText = [
+    "MONDAY AM",
+    "MONDAY PM",
+    "TUESDAY AM",
+    "TUESDAY PM",
+    "WEDNESDAY AM",
+    "WEDNESDAY PM",
+    "THURSDAY AM",
+    "THURSDAY PM",
+    "FRIDAY AM",
+    "FRIDAY PM",
+  ];
+  let found = false;
+  let mappedResults = "";
 
-    const results = useAtomValue(sectionsAtom)
-    const timeslotsText = [  'MONDAY AM',  'MONDAY PM',  'TUESDAY AM',  'TUESDAY PM',  'WEDNESDAY AM',  'WEDNESDAY PM',  'THURSDAY AM',  'THURSDAY PM',  'FRIDAY AM',  'FRIDAY PM']
-    let found = false
-    let mappedResults = ''
+  if (results !== null) {
+    found = true;
+    mappedResults = results.map((item) => ({
+      ...item,
+      timeslot: timeslotsText[item.timeslot_id],
+    }));
+  }
 
-    if (results !== null) {
-        found = true
-         mappedResults = results.map(item => ({
-            ...item,
-            timeslot: timeslotsText[item.timeslot_id],
-        }));
+  const calculateDayColor = (day) => {
+    switch (day) {
+      case "MONDAY AM":
+      case "MONDAY PM":
+        return "badge badge-warning gap-2";
+      case "TUESDAY AM":
+      case "TUESDAY PM":
+        return "badge badge-secondary gap-2";
+      case "WEDNESDAY AM":
+      case "WEDNESDAY PM":
+        return "badge badge-accent gap-2";
+      case "THURSDAY AM":
+      case "THURSDAY PM":
+        return "badge badge-warning badge-outline gap-2";
+      case "FRIDAY AM":
+      case "FRIDAY PM":
+        return "badge badge-info gap-2";
+      default:
+        return "";
     }
+  };
 
-    const calculateDayColor = (day) => {
-        console.log(day)
-        switch (day) {
-            case 'MONDAY AM':
-            case 'MONDAY PM':
-                return 'badge badge-warning gap-2';
-            case 'TUESDAY AM':
-            case 'TUESDAY PM':
-                return 'badge badge-secondary gap-2';
-            case 'WEDNESDAY AM':
-            case 'WEDNESDAY PM':
-                return 'badge badge-accent gap-2';
-            case 'THURSDAY AM':
-            case 'THURSDAY PM':
-                return 'badge badge-warning badge-outline gap-2';
-            case 'FRIDAY AM':
-            case 'FRIDAY PM':
-                return 'badge badge-info gap-2';
-            default:
-                return '';
-        }
-    }
+  const handleAddCourse = (course) => {
+    setCourseCart((current) => [...current, course]);
+  };
 
-    return (
-        <div className="overflow-x-auto my-4">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Section No.</th>
-                        <th>Course Code</th>
-                        <th>Course Name</th>
-                        <th>Status</th>
-                        <th>Timeslot</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                { found == false
-                ? <div> No open sections found </div>
-                : mappedResults.map((item, index) => (
-                        <tr key={item.section_id}>
-                            <th> {item.section_number }</th>
-                            <td> {item.course_code}</td>
-                            <td> {item.course_name}</td>
-                            <td>
-                                {item.current_students < item.max_students
-                                    ? <div className="badge badge-success gap-2">Available</div>
-                                    : item.current_students === item.max_students
-                                        ? <div className="badge badge-warning gap-2">Almost Full</div>
-                                        : <div className="badge badge-error gap-2">Full</div>
-                                }
-                            </td>
-                            <td> <div className={calculateDayColor(item.timeslot)}>{item.timeslot}</div> </td>
-                            <td>  <button className="btn btn-xs">Add</button> </td>
-                        </tr>
-                    ))}
+  useEffect(() => {
+    sessionStorage.setItem("courseCart", JSON.stringify(courseCart));
+  }, [courseCart]);
 
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  return (
+    <div className="overflow-x-auto my-4">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Section No.</th>
+            <th>Course Code</th>
+            <th>Course Name</th>
+            <th>Status</th>
+            <th>Timeslot</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {found === false
+            ? null
+            : mappedResults.map((item, index) => (
+                <tr key={item.section_id}>
+                  <th> {item.section_number}</th>
+                  <td> {item.course_code}</td>
+                  <td> {item.course_name}</td>
+                  <td>
+                    {item.current_students < item.max_students ? (
+                      <div className="badge badge-success gap-2">Available</div>
+                    ) : item.current_students === item.max_students ? (
+                      <div className="badge badge-warning gap-2">
+                        Almost Full
+                      </div>
+                    ) : (
+                      <div className="badge badge-error gap-2">Full</div>
+                    )}
+                  </td>
+                  <td>
+                    <div className={calculateDayColor(item.timeslot)}>
+                      {item.timeslot}
+                    </div>
+                  </td>
+                  <td>
+                    {courseCart.find(
+                      (section) => section.section_id === item.section_id,
+                    ) ? (
+                      <button className="btn btn-xs" disabled>
+                        Added
+                      </button>
+                    ) : (
+                      <button
+                        className="btn btn-xs"
+                        onClick={() => handleAddCourse(item)}
+                      >
+                        Add to Shortlist
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default SearchResult
+export default SearchResult;
