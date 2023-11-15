6359d0a4948a41a06254b248445b652bff6c3ace
bp40 <60870525+bp40@users.noreply.github.com>
2023-11-15 23:57:30 +0700

    fix type error []unit8 to time

--- server/models/student.go
+++ server/models/student.go
@@ -1,15 +1,13 @@
 package models
 
-import "time"
-
 type WebStudent struct {
-	StudentId   int       `json:"StudentId" db:"student_id"`
-	FirstName   string    `json:"first_name" db:"first_name"`
-	LastName    string    `json:"last_name" db:"last_name"`
-	Sex         string    `json:"sex" db:"sex"`
-	DateOfBirth time.Time `json:"date_of_birth" db:"date_of_birth"`
-	EnrollYear  int       `json:"enroll_year" db:"enroll_year"`
-	Level       string    `json:"level" db:"level"`
+	StudentId   int    `json:"StudentId" db:"student_id"`
+	FirstName   string `json:"first_name" db:"first_name"`
+	LastName    string `json:"last_name" db:"last_name"`
+	Sex         string `json:"sex" db:"sex"`
+	DateOfBirth string `json:"date_of_birth" db:"date_of_birth"`
+	EnrollYear  int    `json:"enroll_year" db:"enroll_year"`
+	Level       string `json:"level" db:"level"`
 }
 
 type Student struct {
